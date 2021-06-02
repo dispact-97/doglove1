6a534e3ec29d48fe6e40ff7374c5a1e15b9a2a29
dispact-97 <82069566+dispact-97@users.noreply.github.com>
2021-06-02 10:12:34 +0900

    more info update

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,13 @@
     <nav class="breadcrumb">
   <ul>
 <li><nuxt-link to="/"><i class="fas fa-home"></i>홈으로</nuxt-link></li>
+<li><nuxt-link to="tripname"><i class="fas fa-info"></i>여행이름</li>
   </ul>
 </nav>
     <nuxt/>
     <footer class="footer">
        <div class="content has-text-centered">
           <i class="far fa-copyright"></i>2020-2021 모든저작권은 <strong>여행일지</strong>에 있습니다.
-
          </div>
     </footer>
   </div>
---
+++ pages/tripname.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <section class="hero is-link">
+            <div class="hero-body">
+                <i class="trip info1"></i>
+                    <i class="trip info2"></i>
+                </div>
+        </section>
+    </div>
+    
+</template>
