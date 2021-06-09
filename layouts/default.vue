53a7a683dde875515b93cc7f227acd0815a38a9e
dispact-97 <82069566+dispact-97@users.noreply.github.com>
2021-06-09 09:43:47 +0900

    read me commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <nav class="breadcrumb">
-  <ul>
-<li><nuxt-link to="/"><i class="fas fa-home"></i>홈으로</nuxt-link></li>
-<li><nuxt-link to="/tripname"><i class="fas fa-info"></i>나라별 수도</nuxt-link></li>
-  </ul>
+        <ul>
+            <li><nuxt-link to="/"><i class="fas fa-home"></i>홈으로</nuxt-link></li>
+            <li><nuxt-link to="/tripname"><i class="fas fa-info"></i>나라별 수도</nuxt-link></li>
+            <li><nuxt-link to="/countryBreeds"><i class="fas fa-suitcase-rolling"></i>나라별 관광지</nuxt-link></li>
+        </ul>
 </nav>
     <nuxt/>
     <footer class="footer">
        <div class="content has-text-centered">
-          <i class="far fa-copyright"></i>2020-2021 모든저작권은 <strong>여행일지</strong>에 있습니다.
+          <i class="far fa-copysright"></i>2020-2021 모든저작권은 <strong>여행일지</strong>에 있습니다.
          </div>
     </footer>
   </div>
---
+++ pages/countryBreeds.vue
@@ -0,0 +1,22 @@
+<template >
+    <div>
+        <section class="hero is-warning">
+            <div class="hero-body">
+                <p class="title">
+             <i class="fas fa-suitcase-rolling"></i>나라별 관광지
+                </p>
+                    <p calss="subtitle">
+                        나라별 유명한 관광지를 알아보자
+                    </p>
+            </div>
+        </section>
+        <hr />
+            <section class="columns"></section>
+            <div class="columns"></div>
+            <div class="columns"></div>
+
+
+
+    </div>
+    
+</template>
