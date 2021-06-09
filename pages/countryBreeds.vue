42281a8bd8fcf17055e5696104b15ae50a435b45
dispact-97 <82069566+dispact-97@users.noreply.github.com>
2021-06-09 12:47:16 +0900

    update again

--- pages/countryBreeds.vue
+++ pages/countryBreeds.vue
@@ -1,68 +1,71 @@
-<template >
-    <div>
-        <section class="hero is-warning">
-            <div class="hero-body">
-                <p class="title">
-             <i class="fas fa-suitcase-rolling"></i>나라별 관광지
-                </p>
-                    <p calss="subtitle">
-                        나라별 유명한 관광지를 알아보자
-                    </p>
-            </div>
-        </section>
-        <hr />
-            <section class="columns"></section>
-            <div class="columns">
-                    <table class="table">
-                        <thead>
-                            <th>나라이름</th>
-                        </thead>
-                        <tbody>
-                                <template v-for= "pos in tablecountryKeys.length">
-                                    <tr :key="pos">
-                                        <td>{{pos}}</td>
-                                        <td>{{tablecountryKeys[pos -1]}}</td>
-                                        <td>
-                                            <span class="tag is black" 
-                                            v-if= "((tablecountryBreeds[tablecountryBreeds[pos - 1]]), ar.length == 0)">
-                                                없음
-                                            </span>
-                                            <span v-else>
-                                                <template v-for= "subbreed in ar">
-                                                    <span class="tag is-sucess" :key="subbreed">
-                                                    </span>
-                                                </template>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                </template>
-
-                        </tbody>
-                        </table>
-                        <div class="content">
+<template>
+	<div>
+		<section class="hero is-warning">
+			<div class="hero-body">
+				<p class="title">
+                    <i class="fas fa-suitcase-rolling"></i> 나라별 관광지</p>
+				<p class="subtile">
+					나라별 관광지를 알아보자
+				</p>
+			</div>
+		</section>
+		<hr />
+		<section class="columns"></section>
+		<div class="column">
+			<table class="table">
+				<thead>
+					<th>번호</th>
+					<th>나라 이름</th>
+					<th>관광지 이름</th>
+				</thead>
+				<tbody>
+					<template v-for="pos in tablecountryKeys.length">
+						<tr :key="pos">
+							<td>{{ pos }}</td>
+							<td>{{ tablecountryKeys[pos - 1] }}</td>
+							<td>
+								<span class="tag is-black"
+									v-if="
+((ar = tableCountrybreeds[tablecountryKeys[pos - 1]]),ar.length == 0)">
+                                    없음
+                                    </span>
+								<span v-else>
+                                    <template v-for="subbreed in ar">
+                                        <span class="tag is-success" :key="subbreed">
+                                            {{
+											subbreed
+										    }}
+                                        </span>
+                                        &nbsp;</template>
+								</span>
+							</td>
+						</tr>
+					</template>
+				</tbody>
+			</table>
+			<div class="content">
 				<p class="tag is-danger">출처</p>
-				<a href="https://restcountries.eu/rest/v2/all/">Rest country</a><br/>
+				<a href="https://restcountries.eu/#api-endpoints-all">Rest Countries</a><br />
 				<a
-                    class="button is-primary is-small"
+					class="button is-primary is-small"
 					href="https://restcountries.eu/rest/v2/all"
 					>List all breeds</a
 				>
 			</div>
-            </div>
-            <div class="columns"></div>
-    </div>
+		</div>
+		<div class="column"></div>
+	</div>
 </template>
-
 <script>
-    import axios from 'axios';
-    export default{
-       async asyncdData(){
-           const countryBreeds = axios.get('https://restcountries.eu/rest/v2/all');
-            //alert(Object.keys(countryBreeds));
-            return{
-                tablecountryBreeds : countryBreeds.data.name,
-                tablecountryKeys : Object.Keys(countryBreeds.data.name),
-            };
-        },
-    };
+	import axios from 'axios';
+	export default {
+		async asyncData() {
+			const countryBreeds = await axios.get('https://dog.ceo/api/breeds/list/all');
+			//alert(Object.keys(countryBreeds));
+			return {
+				tableCountrybreeds: countryBreeds.data.message,
+				tablecountryKeys: Object.keys(countryBreeds.data.message),
+			};
+		},
+	};
 </script>
--- update.bat
+++ update.bat
@@ -1,3 +1,5 @@
+chcp 65001
+
 git add --all
 git commit -m "update again"
 git push -u origin master
