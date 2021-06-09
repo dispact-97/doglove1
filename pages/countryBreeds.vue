4d9cdab14cadd8cb8caca7ad56dac1c77e23d61a
dispact-97 <82069566+dispact-97@users.noreply.github.com>
2021-06-09 10:47:55 +0900

    add axios module

--- pages/countryBreeds.vue
+++ pages/countryBreeds.vue
@@ -12,7 +12,39 @@
         </section>
         <hr />
             <section class="columns"></section>
-            <div class="columns"></div>
+            <div class="columns">
+                    <table class="table">
+                        <thead>
+                            <th>나라이름</th>
+                        </thead>
+                        <tbody>
+                                <template v-for= "pos in tablecountryKeys.length">
+                                    <tr :key="pos">
+                                        <td>{{pos}}</td>
+                                        <td>{{tablecountryKeys[pos -1]}}</td>
+                                        <td>
+                                            <span class="tag is black" 
+                                            v-if= "((tablecountryBreeds[tablecountryBreeds[pos - 1]]), ar.length == 0)">
+                                                없음
+                                            </span>
+                                            <span v-else>
+                                                <template v-for= "subbreed in ar">
+                                                    <span class="tag is-sucess" :key="subbreed">
+                                                    </span>
+                                                </template>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </template>
+
+                        </tbody>
+                        </table>
+                        <div class="content">
+
+
+            </div>
+
+
             <div class="columns"></div>
 
     </div>
@@ -22,6 +54,13 @@
 <script>
     import axios from 'axios';
     export default{
-
+       async asyncdData(){
+           const countryBreeds = axios.get('https://restcountries.eu/rest/v2/all');
+            //alert(Object.keys(countryBreeds));
+            return{
+                tablecountryBreeds : countryBreeds.data.name,
+                tablecountryKeys : Object.Keys(countryBreeds.data.name),
+            };
+        },
     };
 </script>
